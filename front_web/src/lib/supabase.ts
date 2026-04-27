b7a6964dc47cd02b28b4aea94a1d6f5875302b08
unknown <MyHome@DESKTOP-23UACM3.lgcns.com>
2026-04-27 17:24:51 +0900

    feat:web local implement

--- front_web/src/lib/supabase.ts
+++ front_web/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+const SUPABASE_URL = import.meta.env.SUPABASE_URL as string
+const SUPABASE_KEY = import.meta.env.SUPABASE_KEY as string
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
